609202844fe1bacf981ab31c2ad4ec3dd3f43a21
CalsonicSSS <calson.developer@gmail.com>
2025-03-13 13:25:46 -0400

    fix lint 2

--- src/components/Doc/HighlightText.tsx
+++ src/components/Doc/HighlightText.tsx
@@ -1,14 +1,11 @@
 import React, { ReactNode } from 'react';
-import { ScrollManager } from './utils';
 
 export default function HighlightText({ content, clickToId }: { content: string; clickToId?: string }): ReactNode {
   return (
     <span
       className={`text-navy-0 font-bold ${clickToId ? 'hover: cursor-pointer' : ''} `}
       onClick={() => {
-        if (clickToId) {
-          ScrollManager.scrollToElement(clickToId);
-        }
+        console.log(clickToId);
       }}
     >
       {content}
